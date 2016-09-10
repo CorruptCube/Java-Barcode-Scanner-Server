25ace4057d113d3ffcc5e43956b16150d74c9de1
CorruptCube <gamerkcw123@hotmail.com>
2016-09-10 11:46:26 -0700

    set tray icon size
    Using a 48x48px icon.  scales fine in OS X but currently looks as
    though windows is scaling down to 16x16px…

--- src/wetsch/jbcsserver/gui/SystemTrayIcon.java
+++ src/wetsch/jbcsserver/gui/SystemTrayIcon.java
@@ -43,7 +43,7 @@ public class SystemTrayIcon extends TrayIcon {
 	//Setup tray icon.
 	private void setupTrayIcon() throws AWTException{
 		tray = SystemTray.getSystemTray();
-		setImageAutoSize(false);
+		setImageAutoSize(true);
 		tray.add(this);
 	}
 	
